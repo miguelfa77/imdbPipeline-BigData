02a1e0d7b029b74efd7c0d41de323c86a98856a4
miguelfa77 <miguelfa77@gmail.com>
2024-03-16 18:28:06 +0100

    wait_for_mysql and retries fix

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ A simple ETL Pipeline for ingesting imdb datasets into mySQL using Docker.
 
 ### Integration Next Steps:
 - Add Schema with keys to create ER model (Priority: High)
-- Add dataclass for cleanness/readability (Priority: Low)
+- Add dataclass to store url's for cleanness/readability (Priority: Low)
 - Add logger (Priority: Low)
--- etl/etl_script.py
+++ etl/etl_script.py
@@ -1,25 +1,42 @@
 import pandas as pd
-import os
+from sqlalchemy import create_engine
 import mysql.connector
 import requests
 import io
+import time
 
 print("Initiating ETL pipeline...")
 
-
-destination_mysql_config = {
+destination_config = {
     'dbname': 'imdb',
     'user': 'user',
     'password': 'password',
     'host': 'destination_mysql'
 }
 
-conn = mysql.connector.connect(
-    user=destination_mysql_config['user'],
-    password=destination_mysql_config['password'],
-    host=destination_mysql_config['host'],
-    database=destination_mysql_config['dbname']
-)
+def wait_for_mysql(host, max_retries=5, delay_seconds=5):
+    """Wait for mySQL to become available."""
+    retries = 0
+    while retries < max_retries:
+        try:
+            engine = create_engine(
+                f'mysql+mysqlconnector://{destination_config['user']}:{destination_config['password']}@{destination_config['host']}/{destination_config['dbname']}')
+            print("Successfully connected to mySQL")
+            return engine
+
+        except Exception as e:
+            print("Error connecting to mySQL:", e)
+            retries += 1
+            print(
+                f"Retrying in {delay_seconds} seconds... (Attempt {retries}/{max_retries})")
+            time.sleep(delay_seconds)
+    print("Max retries reached. Exiting.")
+    return False
+
+engine = wait_for_mysql(host="destination_mysql")
+if not engine:
+    exit(1)
+
 
 base_url = 'https://datasets.imdbws.com/'
 file_names = [
@@ -36,9 +53,11 @@ for file_name in file_names:
     url = f'https://datasets.imdbws.com/{file_name}.tsv.gz'
     response = requests.get(url)
     df = pd.read_csv(io.BytesIO(response.content), compression='gzip', sep='\t', low_memory=False, dtype='str', na_values="\\N")
-    df.to_sql(name=file_name, engine=conn, if_exists='replace')
-    print(f'Filename {file_name}: Converted to SQL')
-
+    try:
+        df.to_sql(name=file_name, con=engine, if_exists='replace')
+        print(f'Filename {file_name}: Converted to SQL')
+    except Exception as e:
+        print("Error when converting df to mySQL:", e)
 
 
 print("Successfully ran ETL pipeline")
